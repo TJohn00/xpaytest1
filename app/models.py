06211138f4bd5eb8f0f6a896ed15a7afce59197b
Thomas <thomas.ben@nestgroup.net>
2023-09-27 16:30:44 +0530

    fixes

--- app/models.py
+++ app/models.py
@@ -1,14 +1,16 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from app.db import engine
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String)
+    full_name = Column(String)
     email = Column(String, unique=True, index=True)
     phone = Column(String, unique=True)
     password = Column(String)
     profile_picture_id = Column(String, nullable=True)
+
+Base.metadata.create_all(bind=engine)
--- app/routers/user.py
+++ app/routers/user.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 @router.post("/register/")
 async def register_user(
-    first_name: str = Form(...),
+    full_name: str = Form(...),
     email: str = Form(...),
     password: str = Form(...),
     phone: str = Form(...),
@@ -19,7 +19,7 @@ async def register_user(
         with SessionLocal() as db:
             profile_picture_data = await profile_picture.read()
             new_user = create_user_db(
-                db, first_name, email, phone, password, profile_picture_data)
+                db, full_name, email, phone, password, profile_picture_data)
             return {"user_id": new_user.id, "message": "User registered successfully"}
     except HTTPException as e:
         raise e
@@ -41,16 +41,16 @@ def get_user(user_id: int):
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
-def create_user_db(db: Session, first_name: str, email: str, phone: str, password: str, profile_picture_data: bytes) -> User:
-    existing_user = db.query(User).filter(User.email == email).first()
+def create_user_db(db: Session, full_name: str, email: str, phone: str, password: str, profile_picture_data: bytes) -> User:
+    existing_user = db.query(User).filter((User.email == email) | (User.phone == phone)).first()
     if existing_user:
         raise HTTPException(
-            status_code=400, detail="Email already registered")
+            status_code=400, detail="Email or phone already registered")
     profile_picture_id = str(ObjectId())
     mongo_collection.insert_one(
         {"_id": profile_picture_id, "picture": profile_picture_data})
     new_user = User(
-        first_name=first_name,
+        full_name=full_name,
         email=email,
         phone=phone,
         password=password,
